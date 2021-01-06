ad7361a8d1f668f01167b0aa16860f8ab5230033
pavlin-dimitrov <74736322+pavlin-dimitrov@users.noreply.github.com>
2021-01-06 17:55:21 +0200

    max() function fixed

--- Project_Test/test.php
+++ Project_Test/test.php
@@ -9,22 +9,30 @@ $hex_splited = [
 		['F0', '1A', 'C3'],//   165, 166, 167
 		['0F', 'FE', 'F4'],
 	   ];
+
+
 $arr_rgb = [];
 $count2 = count($hex_splited);
-
+$max = max(array_map("max", $hex_splited));
+$key = 0;
 for ($i=0; $i < $count2; $i++) { 
 	$inn_count = count($hex_splited[$i]);
 	for ($j=0; $j < $inn_count; $j++) { 
  			$val = hexdec($hex_splited[$i][$j]);
  			$arr_rgb[$i][$j] = $val;
+ 			if ($max == $hex_splited[$i][$j]) {
+ 				$key = $i;
+ 			}
  	}
-}	
+}
 /* За всеки ред от масива намери най-голямата стойност и върни нейния ключ, 
 	ако този ключ е = на 1, то принтирай индекса на реда; */
-$max = max($arr_rgb);
+//$max = max(array_map("max", $arr_rgb));
 var_dump($max);
-$key = array_search($max, $arr_rgb);
-echo $key;
+//$key = array_search($max, $hex_splited);
+echo "<pre>";
+echo "array key is: " . $key;
+echo "</pre>";
 
 	echo "<table border='1'>";
 for ($m=0; $m < $count2; $m++) {
