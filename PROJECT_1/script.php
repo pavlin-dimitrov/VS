4d2c46e4784ae183a93782a17d4c3e82ce3c2a8e
pavlin-dimitrov <74736322+pavlin-dimitrov@users.noreply.github.com>
2021-01-07 16:24:42 +0200

    find max for each sub array with key 1 - done

--- PROJECT_1/script.php
+++ PROJECT_1/script.php
@@ -1,38 +1,31 @@
 <?php
-if (!isset($_GET['colors'])) {
+if (!isset($_POST['colors'])) {
 
 } else {
-	$replace_quot = str_replace('"', "", $_GET['colors']);  // Remove the quotes from the string;
+	$replace_quot = str_replace('"', "", $_POST['colors']);  // Remove the quotes from the string;
 	$remove_spaces = preg_replace('/\s+/', '', $replace_quot); // Remove spaces from the string;
 	$hex_colors_arr = explode(',', $remove_spaces, pow(10, 4)); // Separate the string on elements of an array;	
 	$count = count($hex_colors_arr);	
 	$hex_splited = [];
-	$arr_rgb = [];
 	
 		foreach ($hex_colors_arr as $key => $value) {  //За всеки елемент в масива
 			$hex_colors_arr = str_split($value, 2);		   // Раздели елемента на двойки		
 			$hex_splited[] = $hex_colors_arr;
 		}
-		$count2 = count($hex_splited);
-		for ($i=0; $i < $count2; $i++) { 
-			for ($j=0; $j < 3; $j++) { 
-	 				$val = hexdec($hex_splited[$i][$j]);
-	 				$arr_rgb[$i][$j] = $val;
-	 		} 
-		}	
-}
-$max = max($arr_rgb);
-var_dump($max);
-$key = array_search($max, $arr_rgb);
-echo $key;
-echo "<table border='1'>";
-for ($m=0; $m < $count; $m++) {
-	echo "<tr>";
-	for ($k=0; $k < 3; $k++) { 
-		echo "<td>";
-		echo $arr_rgb[$m][$k];
-		echo "</td>";
+	$resultNone = [];
+	$resultMax = [];
+		foreach ($hex_splited as $key => $value) {
+  			if(count(array_count_values($value)) < 3 && hexdec($value[1]) == max(array_map('hexdec', $value))) {
+   				$resultNone[] = $key;
+   			} elseif (count(array_count_values($value)) != 1 && hexdec($value[1]) == max(array_map('hexdec', $value))) {
+   				$resultMax[] = $key;
+   			}
+		 }
+	$count_resultMax = count($resultMax);
+
+echo "[";
+	for ($m=0; $m < $count_resultMax; $m++) {
+		echo $resultMax[$m] . ",";
 	}
-	echo "</tr>";
+echo "]";
 }
-echo "</table>";
--- Project_Test/test.php
+++ Project_Test/test.php
@@ -1,47 +1,20 @@
 <?php
-/* Трансформирането на разделения по двойки HEX код в RGB код.
-Печатане в таблица.
-Следваща стъпка е търсене в масива ако за всеки i елемент на всеки под масив
-key1 е по-голям от key0 и key2, то тогава принтирай ми индекса на масива. */
 $hex_splited = [
-		['00','00','00'],    // 242, 240, 160
-		['10', '11', '10'],	//  241, 241, 241
-		['F0', '1A', 'C3'],//   165, 166, 167
+		['00','00','00'], 
+		['10', '11', '10'],	
+		['F0', '1A', 'C3'],
 		['0F', 'FE', 'F4'],
 	   ];
+$resultMax = [];
+foreach ($hex_splited as $key => $value) {
+  	if(count(array_count_values($value)) != 1 && hexdec($value[1]) == max(array_map('hexdec', $value))) {
+   		$resultMax[] = $key;
+   	}
+ }
+$count_resultMax = count($resultMax);
 
-
-$arr_rgb = [];
-$count2 = count($hex_splited);
-$max = max(array_map("max", $hex_splited));
-$key = 0;
-for ($i=0; $i < $count2; $i++) { 
-	$inn_count = count($hex_splited[$i]);
-	for ($j=0; $j < $inn_count; $j++) { 
- 			$val = hexdec($hex_splited[$i][$j]);
- 			$arr_rgb[$i][$j] = $val;
- 			if ($max == $hex_splited[$i][$j]) {
- 				$key = $i;
- 			}
- 	}
-}
-/* За всеки ред от масива намери най-голямата стойност и върни нейния ключ, 
-	ако този ключ е = на 1, то принтирай индекса на реда; */
-//$max = max(array_map("max", $arr_rgb));
-var_dump($max);
-//$key = array_search($max, $hex_splited);
-echo "<pre>";
-echo "array key is: " . $key;
-echo "</pre>";
-
-	echo "<table border='1'>";
-for ($m=0; $m < $count2; $m++) {
-	echo "<tr>";
-	for ($k=0; $k < $inn_count; $k++) { 
-		echo "<td>";
-		echo $arr_rgb[$m][$k];
-		echo "</td>";
+echo "[";
+	for ($m=0; $m < $count_resultMax; $m++) {
+		echo $resultMax[$m] . ",";
 	}
-	echo "</tr>";
-}
-echo "</table>";
+echo "]";
