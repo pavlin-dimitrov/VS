f1a91b12d0f452410278777b32415820c1a0b989
pavlin-dimitrov <74736322+pavlin-dimitrov@users.noreply.github.com>
2021-01-16 12:11:19 +0200

    final script code
    final script code;
    some teste;
    task 6 from 14.01.21

--- PROJECT_1/script.php
+++ PROJECT_1/script.php
@@ -1,39 +1,29 @@
 <?php
 if (!isset($_POST['colors'])) {
-
 } else {
 	if (empty($_POST['colors'])){
-		$_SESSION['error_colors'] = 'Colors are required!';
-		echo "<a href='session_destroy.php'>Enter some HEX colors. Please try again!</a>";
+		echo "<a href='index.html'>Enter some HEX colors. Please try again!</a>";
 	}else{
-		$replace_quot = str_replace('"', "", $_POST['colors']);  // Remove the quotes from the string;
-		$remove_spaces = preg_replace('/\s+/', '', $replace_quot); // Remove spaces from the string;
-		$hex_colors_arr = explode(',', $remove_spaces, pow(10, 4)); // Separate the string on elements of an array;	
-		$count = count($hex_colors_arr);	
-		$hex_splited = [];
-	
+		$hex_colors_arr = str_replace('"', "", preg_replace('/[\[(\s+)\]]/', '', explode(',', $_POST['colors'], pow(10, 4))));	//cut spaces,sq. br. and quotes
+		$hex_splited = []; //empty array for the splited arrayof hex color strings
 			foreach ($hex_colors_arr as $key => $value) {  //For each element in the array
 				$hex_colors_arr = str_split($value, 2);	   // Split the element by 2 char's		
 				$hex_splited[] = $hex_colors_arr;		   // Enter the new elements in a new array
 			}
-		$resultNone = [];
+
 		$resultMax = [];
 			foreach ($hex_splited as $key => $value) {
-  				if(count(array_count_values($value)) < 3 && hexdec($value[1]) == max(array_map('hexdec', $value))) {
-   					$resultNone[] = $key;
-   				} elseif (count(array_count_values($value)) != 1 && hexdec($value[1]) == max(array_map('hexdec', $value))) {
-   					$resultMax[] = $key;
-   				}
+				if (hexdec($value[1]) > hexdec($value[0]) && hexdec($value[1]) > hexdec($value[2]) ) {
+					$resultMax[] = $key;
+				}
 		 	}
 		$count_resultMax = count($resultMax);
-
 	echo "[";
 		for ($m=0; $m < $count_resultMax; $m++) {
 			echo $resultMax[$m] . ",";
 		}
 	echo "]";
-	echo "<pre>";
-	echo "<a href='session_destroy.php'>Check other colors!</a>";
-	echo "</pre>";
+	echo "<br>";
+	echo "<a href=header(Location: 'index.html')>Check other colors!</a>";
 	}
 }
--- Project_Test/page.php
+++ Project_Test/page.php
@@ -5,12 +5,13 @@
 	<title>Document</title>
 </head>
 <body>
-	<form action="script_tests.php" method="get">
+
+	<form action="script_tests.php" method="post">
 	<p>HEX Colors here:</p>
 	<input type="colors" name="colors">
 	<p>CHECH IF ANY</p>
 	<input type="submit" name="submit" value="CHECK">
 </form>
-	
+
 </body>
 </html>
